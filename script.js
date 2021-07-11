0655a5ba6974cb421fd2854f04e13a1b5987b6e8
Deesjes <85071119+Deesjes@users.noreply.github.com>
2021-07-11 20:17:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+const myAge = 34;
+const isFemale = true;
+const driverStatus = "bob";
+const firstName = "Daisy";
+const totalAmount = 25;
+
+
+if (myAge >= 18) {
+
+    console.log("You are allowed to enter");
+
+} else {
+    console.log("you are to young, please come back when you have turned 18");
+
+}
+
+if (isFemale) {
+
+    console.log("You are female")
+} else {
+
+    console.log("you are male")
+}
+
+
+if (driverStatus === "bob") {
+    console.log("you are allowed to drive.");
+
+} else {
+    console.log("you are drunk, call a cab.");
+
+}
+
+if (myAge >= 18 && myAge <= 25) {
+
+    console.log("jij krijgt 50% korting")
+
+} else {
+    console.log("Helaas geen korting voor jou")
+
+}
+
+if (firstName) {
+
+    console.log("gratis pils voor jou")
+}
+
+if (totalAmount === 25 || totalAmount === 50 || totalAmount === 100) {
+
+    console.log("gratis bitterballen voor jou")
+
+} else {
+    console.log
+}
